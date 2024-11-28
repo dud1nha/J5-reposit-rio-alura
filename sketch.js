27dbd377945d91af63e94d465a30d6407544df53
dud1nha <mariacarvalhoduda912@gmail.com>
2024-11-28 08:58:44 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,86 @@
+deixe palavra ;
+
+
+configuração de função () {
+
+criarCanvas (400, 400); 
+
+
+
+palavra = palavraAleatoria ();
+
+}
+
+
+
+
+
+função palavraAleatoria (){
+
+let palavras = [ "Caminhante", "Caminho", "Caminha"];
+
+
+
+retornar aleatório (palavras );
+
+}
+
+
+
+240
+
+:
+
+:
+
+função inicializaCores () {
+
+
+
+
+
+preencher ("preto");
+
+
+
+
+
+}
+
+
+
+fundo ("branco" );
+
+tamanho do texto (64);
+
+textAlign (CENTRO CENTRO );
+
+function palavraParcial (mínimo, máximo ) {
+
+
+
+deixe quantidade minimo maximo, 1 = mapa (mouseX palavra length );
+
+deixe parcial = palavra substring(0, quantidade );
+
+
+
+
+
+}
+
+
+
+retornar parcial;
+
+função draw () {
+
+inicializaCores ();
+
+
+deixe texto = largura ); palavraParcial (0,
+
+
+texto (texto, 200, 200);
+
+ }
